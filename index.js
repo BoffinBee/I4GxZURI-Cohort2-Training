195f9349eb3801b219bb3e8293c9c5057740da62
Abolarinwa Quadri <59104498+BoffinBee@users.noreply.github.com>
2022-10-17 15:02:13 +0000

    Added comments to the code

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+// guesser function
 function guess(range) {
   return Math.floor((Math.random() * range) + 1)
 }
 
+// game flow
 function game() {
+  // initialize stage and points counter
   var stage = 1
   var point = 0
+  
+  // start an infinite loop
   while (true) {
     console.log("Stage", stage)
     const sysGuess = guess(stage+1)
     const userGuess = prompt("Guess > ")
+
+    // Update the user's points and stage
+    // accordingly if the guess was right
+    // otherwise, end the game
     if (sysGuess == userGuess) {
       stage += 1
       point += 1
@@ -21,10 +30,12 @@ function game() {
   console.log("Wrong!\nGame over - You finished with", point, "points.") 
 }
 
+// game initializer 
 function start() {
   const username = prompt("Username >")
   console.log("Hey " + username + ", Welcome!\n")
   game()
 }
 
+// start the game
 start()
